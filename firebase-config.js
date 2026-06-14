8e63af6392b6eae14e6d795d7bedd834ceb53326
Daniel Jansen <2021djansen@gmail.com>
2026-06-14 16:35:10 -0400

    Added Firebase import

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,13 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDikhno-Mwim2peFQKbn-avaGQCk59deEP4",
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDkhnO-Mwim2peFQKbn-avaGQCk59deEP4",
   authDomain: "volleyball-stat-tracker-350db.firebaseapp.com",
   projectId: "volleyball-stat-tracker-350db",
   storageBucket: "volleyball-stat-tracker-350db.firebasestorage.app",
@@ -7,3 +15,7 @@ export const firebaseConfig = {
   appId: "1:461139136189:web:34da3f9e33951cd35a71d4",
   measurementId: "G-M23K290LB1"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
