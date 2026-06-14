60e952feb141cedd6dcc51a61a32d72bff68cc5d
danieljansen2003 <2021djansen@gmail.com>
2026-06-14 15:16:02 -0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "volleyball-stat-tracker-350db.firebaseapp.com",
+  projectId: "volleyball-stat-tracker-350db",
+  storageBucket: "volleyball-stat-tracker-350db.firebasestorage.app",
+  messagingSenderId: "461139136189",
+  appId: "YOUR_APP_ID",
+  measurementId: "G-M23K290LB1"
+};
