600a1c37636b02469700cffc4b639d228e2f1e44
Daniel Jansen <2021djansen@gmail.com>
2026-06-14 16:56:02 -0400

    Updated Firebase 2

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "PASTE_API_KEY_HERE",
-  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE_PROJECT_ID.appspot.com",
-  messagingSenderId: "PASTE_SENDER_ID",
-  appId: "PASTE_APP_ID"
+  apiKey: "AIzaSyDkhnO-Mwim2peFQKbn-avaGQCk59deEP4",
+  authDomain: "volleyball-stat-tracker-350db.firebaseapp.com",
+  projectId: "volleyball-stat-tracker-350db",
+  storageBucket: "volleyball-stat-tracker-350db.firebasestorage.app",
+  messagingSenderId: "461139136189",
+  appId: "1:461139136189:web:34da3f9e33951cd35a71d4",
+  measurementId: "G-M23K290LB1"
 };
